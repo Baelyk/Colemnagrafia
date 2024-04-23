165555fe1053aae85f26659f6e1e9ec3d8fbb495
Baelyk <b43lyk@gmail.com>
2024-04-23 16:58:26 +0200

    Puzzle persistence
    Puzzle persistence using the Tauri store plugin. Closes #1.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { invoke } from "@tauri-apps/api/core";
+import { Store } from '@tauri-apps/plugin-store';
 
 const DEBUG = {
   allowAnyWord: false,
@@ -21,13 +22,17 @@ const FONTS = {
   word: "JetBrains Mono, monospace",
 }
 
+interface HasSize {
+  width: number;
+  height: number;
+}
 const SIZES = {
-  smallestDimension: (game: Game) => Math.min(game.height, game.width),
-  big: (game: Game) => SIZES.smallestDimension(game) / 7,
-  medium: (game: Game) => SIZES.smallestDimension(game) / 10,
-  small: (game: Game) => SIZES.smallestDimension(game) / 15,
-  tiny: (game: Game) => SIZES.smallestDimension(game) / 25,
-  teeny: (game: Game) => SIZES.smallestDimension(game) / 90,
+  smallestDimension: (size: HasSize) => Math.min(size.height, size.width),
+  big: (size: HasSize) => SIZES.smallestDimension(size) / 7,
+  medium: (size: HasSize) => SIZES.smallestDimension(size) / 10,
+  small: (size: HasSize) => SIZES.smallestDimension(size) / 15,
+  tiny: (size: HasSize) => SIZES.smallestDimension(size) / 25,
+  teeny: (size: HasSize) => SIZES.smallestDimension(size) / 90,
 }
 
 type WordMap = { [key: string]: string[] };
@@ -41,14 +46,7 @@ interface Game {
 
   errorText: string | null;
 
-  puzzle_promise: Promise<unknown>;
-  letters: string[];
-  words: WordMap;
-  pangrams: Set<string>;
-  maxScore: number;
-  word: string;
-  found: string[];
-  score: number;
+  puzzle: Puzzle;
 
   mouseX: number;
   mouseY: number;
@@ -67,37 +65,33 @@ interface Game {
   wordlistUserIsScrolling: boolean;
 }
 
-type NewPuzzleResponse = [string[], WordMap, string[]];
+interface Puzzle {
+  letters: string[];
+  words: WordMap;
+  pangrams: string[];
+  maxScore: number;
+  word: string;
+  found: string[];
+  score: number;
+}
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
   const game = init();
   if (game == null) {
     console.error("Error initializing game");
     return;
   }
+  // For debugging, log the game
+  console.debug(game);
 
-  game.puzzle_promise.then(msg => {
-    const message = msg as NewPuzzleResponse;
-
-    console.debug(message);
-
-    game.letters = message[0].map(l => l.toUpperCase());
-    game.words = message[1];
-    game.pangrams = new Set(message[2]);
-    game.maxScore = Object.values(game.words).flat().reduce(
-      (sum, word) => sum + scoreWord(word, game), 0);
-
-    // For debugging, log the game
-    console.debug(game);
-
-    if (DEBUG.foundAllWords) {
-      game.found = Object.values(game.words).flat();
-    }
-  }).catch(message => {
+  try {
+    await loadPuzzle(game);
+  } catch (error) {
     console.error("Puzzle generation failed");
-    console.debug(message);
-    game.errorText = JSON.stringify(message);
-  });
+    console.debug(error);
+    game.errorText = JSON.stringify(error);
+  }
+
 
   window.addEventListener("click", (event) => {
     if (DEBUG.eventLogging) console.log("click");
@@ -181,17 +175,17 @@ function resizeCanvas(canvas: HTMLCanvasElement, width: number, height: number) 
   ctx.scale(ratio, ratio);
 }
 
-function init(): Game | undefined {
+function init(): Game {
   console.log("Initializing...");
   const canvas = document.querySelector("canvas");
   if (canvas == null) {
     console.error("Unable to get canvas");
-    return;
+    throw new Error("Unable to get canvas");
   }
   const ctx = canvas.getContext("2d");
   if (ctx == null) {
     console.error("Unable to get canvas context");
-    return;
+    throw new Error("Unable to get canvas context");
   }
 
   const scaling = window.devicePixelRatio;
@@ -199,7 +193,15 @@ function init(): Game | undefined {
   const height = window.innerHeight;
   resizeCanvas(canvas, width, height);
 
-  const puzzle_promise = invoke("daily_puzzle");
+  const puzzle: Puzzle = {
+    letters: [],
+    words: {},
+    pangrams: [],
+    maxScore: 0,
+    word: "",
+    found: [],
+    score: 0,
+  };
 
   return {
     width,
@@ -209,14 +211,7 @@ function init(): Game | undefined {
 
     errorText: null,
 
-    puzzle_promise,
-    letters: [],
-    words: {},
-    pangrams: new Set(),
-    maxScore: 0,
-    word: "",
-    found: [],
-    score: 0,
+    puzzle,
 
     mouseX: -1,
     mouseY: -1,
@@ -235,6 +230,57 @@ function init(): Game | undefined {
   };
 }
 
+async function savePuzzle(game: Game) {
+  console.log("Saving puzzle state");
+  const store = new Store("store.dat");
+  await store.set("puzzle", game.puzzle);
+  // Manually save the store now as well (instead of hoping for a graceful exit)
+  await store.save();
+}
+
+async function loadPuzzle(game: Game) {
+  const store = new Store("store.dat");
+  let storedPuzzle: Puzzle | null = null;
+  let storedPuzzleDate: string | null = null;
+
+  try {
+    console.log("Loading puzzle...");
+    storedPuzzle = await store.get<Puzzle>("puzzle");
+    storedPuzzleDate = await store.get<string>("puzzle-date");
+  } catch (error) {
+    console.error("Failed to get stored puzzle:");
+    console.error(error);
+  }
+
+  const today = new Date(Date.now()).toDateString();
+  if (storedPuzzleDate != null && storedPuzzle != null) {
+    console.debug(`Loaded stored puzzle state from ${storedPuzzleDate}`);
+    console.debug(storedPuzzle);
+    if (storedPuzzleDate === today) {
+      console.log("Stored puzzle is from today, using it");
+      game.puzzle = storedPuzzle;
+      return;
+    }
+  }
+
+  console.log("Creating a new puzzle...");
+  const puzzle = await invoke("daily_puzzle") as [string[], WordMap, string[]];
+  game.puzzle.letters = puzzle[0].map(l => l.toUpperCase());
+  game.puzzle.words = puzzle[1];
+  game.puzzle.pangrams = puzzle[2];
+  game.puzzle.maxScore = Object.values(game.puzzle.words).flat().reduce(
+    (sum, word) => sum + scoreWord(word, game.puzzle.pangrams), 0);
+  game.puzzle.word = "";
+  game.puzzle.found = [];
+  if (DEBUG.foundAllWords) {
+    game.puzzle.found = Object.values(game.puzzle.words).flat();
+  }
+  game.puzzle.score = 0;
+
+  await store.set("puzzle-date", today);
+  savePuzzle(game);
+}
+
 function main(time: DOMHighResTimeStamp, game: Game) {
   game.ctx.clearRect(0, 0, game.width, game.height);
 
@@ -243,16 +289,13 @@ function main(time: DOMHighResTimeStamp, game: Game) {
     return;
   }
 
-  if (game.letters.length === 0) {
+  if (game.puzzle.letters.length === 0) {
     loading(time, game);
     return;
   }
 
   const clicked = wheel(time, game);
-  game.word += clicked;
-  if (clicked) {
-    console.log(game.word);
-  }
+  game.puzzle.word += clicked;
 
   word(time, game);
 
@@ -272,7 +315,7 @@ function error(_time: DOMHighResTimeStamp, game: Game) {
   game.ctx.fillStyle = COLORS.black;
 
   game.ctx.fillText("Error", SIZES.small(game), SIZES.small(game));
-  game.ctx.font = `${SIZES.small(game)}px ${FONTS.default}`;
+  game.ctx.font = `${SIZES.tiny(game)}px ${FONTS.default}`;
   game.ctx.fillText(game.errorText || "Unknown error", SIZES.small(game), 2 * SIZES.small(game) + SIZES.big(game));
 }
 
@@ -309,7 +352,7 @@ function wheel(time: DOMHighResTimeStamp, game: Game) {
       game.mouseDown = false;
       game.clickedHex = 0;
       game.clickedHexTime = time;
-      clicked = game.letters[0];
+      clicked = game.puzzle.letters[0];
       game.ctx.fillStyle = COLORS.darkyellow;
     }
   }
@@ -331,7 +374,7 @@ function wheel(time: DOMHighResTimeStamp, game: Game) {
   }
   game.ctx.fill();
   game.ctx.fillStyle = COLORS.black;
-  game.ctx.fillText(game.letters[0], centerX, centerY);
+  game.ctx.fillText(game.puzzle.letters[0], centerX, centerY);
 
   // Surrounding hexagons
   const radians = 2 * Math.PI / 6;
@@ -346,7 +389,7 @@ function wheel(time: DOMHighResTimeStamp, game: Game) {
         game.mouseDown = false;
         game.clickedHex = i;
         game.clickedHexTime = time;
-        clicked = game.letters[i];
+        clicked = game.puzzle.letters[i];
         game.ctx.fillStyle = COLORS.darkgray;
       }
     }
@@ -368,7 +411,7 @@ function wheel(time: DOMHighResTimeStamp, game: Game) {
     }
     game.ctx.fill();
     game.ctx.fillStyle = COLORS.black;
-    game.ctx.fillText(game.letters[i], x, y);
+    game.ctx.fillText(game.puzzle.letters[i], x, y);
   }
 
   return clicked;
@@ -381,11 +424,11 @@ function word(_time: DOMHighResTimeStamp, game: Game) {
   game.ctx.font = `bold ${fontsize}px ${FONTS.word}`;
   game.ctx.textBaseline = "middle";
   game.ctx.fillStyle = COLORS.black;
-  let wordWidth = game.ctx.measureText(game.word).width;
+  let wordWidth = game.ctx.measureText(game.puzzle.word).width;
   while (wordWidth > game.width * 0.75) {
     fontsize = fontsize * 0.95;
     game.ctx.font = `bold ${fontsize}px ${FONTS.word}`;
-    wordWidth = game.ctx.measureText(game.word).width;
+    wordWidth = game.ctx.measureText(game.puzzle.word).width;
   }
 
   // If there's a wordMessage, display that instead of the word
@@ -398,10 +441,44 @@ function word(_time: DOMHighResTimeStamp, game: Game) {
 
   game.ctx.textAlign = "left";
   let letterX = game.width / 2 - wordWidth / 2;
-  for (const letter of game.word) {
-    game.ctx.fillStyle = letter === game.letters[0] ? COLORS.yellow : COLORS.black;
+  for (const letter of game.puzzle.word) {
+    game.ctx.fillStyle = letter === game.puzzle.letters[0] ? COLORS.yellow : COLORS.black;
     game.ctx.fillText(letter, letterX, wordY);
     letterX += game.ctx.measureText(letter).width;
+  }
+}
+
+function submitWord(_time: DOMHighResTimeStamp, game: Game) {
+  const enteredWord = game.puzzle.word.toLowerCase();
+  game.puzzle.word = "";
+
+  if (Object.hasOwn(game.puzzle.words, enteredWord)) {
+    if (game.puzzle.found.includes(enteredWord)) {
+      game.wordMessage = "Already found";
+    } else {
+      let count = 0;
+      let score = 0;
+      for (const word of game.puzzle.words[enteredWord]) {
+        game.puzzle.found.unshift(word);
+        score += scoreWord(word, game.puzzle.pangrams);
+        count += 1;
+      }
+      game.puzzle.score += score;
+      game.wordMessage = `+${score / count}${count > 1 ? ` x${count}` : ""}`;
+      savePuzzle(game);
+    }
+  } else if (DEBUG.allowAnyWord) {
+    game.puzzle.found.unshift(enteredWord);
+    const score = scoreWord(enteredWord, game.puzzle.pangrams);
+    game.puzzle.score += score;
+    game.wordMessage = `+${score} !`;
+    savePuzzle(game);
+  } else {
+    if (enteredWord.length < 4) {
+      game.wordMessage = "Too short";
+    } else {
+      game.wordMessage = "Not a word";
+    }
   }
 }
 
@@ -424,7 +501,7 @@ function controls(_time: DOMHighResTimeStamp, game: Game) {
     if (game.mouseDown) {
       game.mouseDown = false;
       game.ctx.fillStyle = COLORS.darkgray;
-      game.word = game.word.substring(0, game.word.length - 1);
+      game.puzzle.word = game.puzzle.word.substring(0, game.puzzle.word.length - 1);
       window.requestAnimationFrame((time) => main(time, game));
     }
   }
@@ -442,7 +519,7 @@ function controls(_time: DOMHighResTimeStamp, game: Game) {
     if (game.mouseDown) {
       game.mouseDown = false;
       game.ctx.fillStyle = COLORS.darkgray;
-      game.letters = game.letters
+      game.puzzle.letters = game.puzzle.letters
         .map((letter, i) => ({ letter, sort: i === 0 ? 0 : Math.random() }))
         .sort((a, b) => a.sort - b.sort)
         .map(({ letter }) => letter);
@@ -463,38 +540,7 @@ function controls(_time: DOMHighResTimeStamp, game: Game) {
       game.mouseDown = false;
       game.ctx.fillStyle = COLORS.darkgray;
 
-      game.word = game.word.toLowerCase();
-      if (Object.hasOwn(game.words, game.word)) {
-        if (game.found.includes(game.word)) {
-          game.wordMessage = "Already found";
-        } else {
-          let count = 0;
-          let score = 0;
-          for (const word of game.words[game.word]) {
-            game.found.unshift(word);
-            score += scoreWord(word, game);
-            count += 1;
-          }
-          game.score += score;
-          game.wordMessage = `+${score / count}${count > 1 ? ` x${count}` : ""}`;
-        }
-      } else if (DEBUG.allowAnyWord) {
-        game.found.unshift(game.word);
-        const score = scoreWord(game.word, game);
-        game.score += score;
-        game.wordMessage = `+${score} !`;
-      } else {
-        if (game.word.length < 4) {
-          game.wordMessage = "Too short";
-        } else {
-          game.wordMessage = "Not a word";
-        }
-
-        if (DEBUG.allowAnyWord) {
-          game.found.unshift(game.word);
-        }
-      }
-      game.word = "";
+      submitWord(_time, game);
 
       window.requestAnimationFrame((time) => main(time, game));
     }
@@ -521,7 +567,7 @@ const SCORERANKS: [number, string][] = [
 
 function scorebar(_time: DOMHighResTimeStamp, game: Game) {
   const rank = SCORERANKS.findIndex(
-    ([minScore, _]) => game.score < Math.round(minScore * game.maxScore)) - 1;
+    ([minScore, _]) => game.puzzle.score < Math.round(minScore * game.puzzle.maxScore)) - 1;
 
   const scorebarWidth = game.width - 2 * SIZES.tiny(game);
   const scorebarX = game.width / 2 - scorebarWidth / 2;
@@ -575,13 +621,13 @@ function scorebar(_time: DOMHighResTimeStamp, game: Game) {
     if (interactingWithTick) {
       // Display min score for rank if interacting
       game.ctx.font = `${SIZES.tiny(game)}px ${FONTS.word}`;
-      game.ctx.fillText(Math.round(game.maxScore * SCORERANKS[i][0]).toString(), tickX, scorebarY);
+      game.ctx.fillText(Math.round(game.puzzle.maxScore * SCORERANKS[i][0]).toString(), tickX, scorebarY);
       // Change rank to this tick's rank while interacting
       displayRank = SCORERANKS[i][1];
     } else if (i === rank) {
       // Display current score if this is the current rank
       game.ctx.font = `bold ${SIZES.tiny(game)}px ${FONTS.word}`;
-      game.ctx.fillText(game.score.toString(), tickX, scorebarY);
+      game.ctx.fillText(game.puzzle.score.toString(), tickX, scorebarY);
     }
 
     if (interactingWithTick) {
@@ -648,7 +694,7 @@ function wordlist(time: DOMHighResTimeStamp, game: Game) {
     }
 
     // Restrict scrolling
-    const rows = Math.ceil(game.found.length / 2) + 1;
+    const rows = Math.ceil(game.puzzle.found.length / 2) + 1;
     // No need to scroll up
     game.wordlistScroll = Math.min(Math.max(0, game.wordlistScroll),
       // No need to bring the end of the list above the bottom
@@ -660,12 +706,12 @@ function wordlist(time: DOMHighResTimeStamp, game: Game) {
     const leftX = wordlistX + wordlistWidth / 20;
     const rightX = wordlistX + wordlistWidth / 2 + wordlistWidth / 20;
     let count = 0;
-    const alphabetical = [...game.found].sort((a, b) => a.localeCompare(b));
+    const alphabetical = [...game.puzzle.found].sort((a, b) => a.localeCompare(b));
     game.ctx.font = `${SIZES.tiny(game)}px ${FONTS.default}`;
-    game.ctx.fillText(`${game.found.length} word${count === 1 ? "" : "s"} found`, leftX, textY);
+    game.ctx.fillText(`${game.puzzle.found.length} word${count === 1 ? "" : "s"} found`, leftX, textY);
 
     for (const word of alphabetical) {
-      game.ctx.font = `${game.pangrams.has(word) ? "bold" : ""} ${SIZES.tiny(game)}px ${FONTS.default}`;
+      game.ctx.font = `${game.puzzle.pangrams.includes(word) ? "bold" : ""} ${SIZES.tiny(game)}px ${FONTS.default}`;
       if (count % 2 === 0) {
         game.ctx.fillText(word, leftX, textY + textHeight * (Math.floor(count / 2) + 1));
       } else {
@@ -692,8 +738,8 @@ function wordlist(time: DOMHighResTimeStamp, game: Game) {
     let previewSize = 0;
     const padding = SIZES.teeny(game);
     game.ctx.fillStyle = COLORS.black;
-    for (const word of game.found) {
-      game.ctx.font = `${game.pangrams.has(word) ? "bold" : ""} ${SIZES.tiny(game)}px ${FONTS.default}`;
+    for (const word of game.puzzle.found) {
+      game.ctx.font = `${game.puzzle.pangrams.includes(word) ? "bold" : ""} ${SIZES.tiny(game)}px ${FONTS.default}`;
       const wordSize = game.ctx.measureText(`${word}`).width;
       if (previewSize + wordSize + padding > wordlistWidth - SIZES.teeny(game) * 2) {
         break;
@@ -705,12 +751,12 @@ function wordlist(time: DOMHighResTimeStamp, game: Game) {
 
 }
 
-function isPangram(word: string, game: Game): boolean {
-  return game.pangrams.has(word);
+function isPangram(word: string, pangrams: string[]): boolean {
+  return pangrams.includes(word);
 }
 
-function scoreWord(word: string, game: Game): number {
-  return word.length + (isPangram(word, game) ? 7 : 0);
+function scoreWord(word: string, pangrams: string[]): number {
+  return word.length + (isPangram(word, pangrams) ? 7 : 0);
 }
 
 
