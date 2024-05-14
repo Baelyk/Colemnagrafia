434a11aff3ace5c294f7e2f40ddab71a4f81929a
Baelyk <b43lyk@gmail.com>
2024-05-14 14:29:26 +0200

    Round logo corners

--- src/logo.ts
+++ src/logo.ts
@@ -12,7 +12,14 @@ export function logo(
 
 	// Background
 	ctx.beginPath();
-	ctx.rect(0, 0, size.width, size.height);
+	roundRect(
+		ctx,
+		0,
+		0,
+		size.width,
+		size.height,
+		SIZES.smallestDimension(size) / 4,
+	);
 	ctx.fillStyle = COLORS.bg({ darkMode });
 	ctx.fill();
 
@@ -33,4 +40,25 @@ export function logo(
 		ctx.fillStyle = COLORS.gray({ darkMode });
 		ctx.fill();
 	}
+}
+
+// Since canvas2svg doesn't implement it
+function roundRect(
+	ctx: CanvasRenderingContext2D,
+	x: number,
+	y: number,
+	width: number,
+	height: number,
+	radius: number,
+) {
+	ctx.beginPath();
+	ctx.moveTo(x + radius, y);
+	ctx.lineTo(x + width - radius, y);
+	ctx.arc(x + width - radius, y + radius, radius, (3 * Math.PI) / 2, 0);
+	ctx.lineTo(x + width, y + height - radius);
+	ctx.arc(x + width - radius, y + height - radius, radius, 0, Math.PI / 2);
+	ctx.lineTo(x + radius, y + height);
+	ctx.arc(x + radius, y + height - radius, radius, Math.PI / 2, Math.PI);
+	ctx.lineTo(x, y + radius);
+	ctx.arc(x + radius, y + radius, radius, Math.PI, (3 * Math.PI) / 2);
 }
