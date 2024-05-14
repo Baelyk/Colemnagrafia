5a6348f0eace59e16aa1544f5e89ba9f0bbafd73
Baelyk <b43lyk@gmail.com>
2024-05-14 19:45:14 +0200

    Update word list generator
    And then recreate palabras.rs and puzzles.json. Allows words whose
    lemma is less than four letters, like "unos" from "un", for #6.

--- crea-explorer/src/generate.rs
+++ crea-explorer/src/generate.rs
@@ -7,62 +7,56 @@ pub fn generate() {
     write_palabras_rs(words, pangrams);
 }
 
-pub fn generate_words_and_pangrams() -> (HashSet<(String, String)>, HashSet<String>) {
-    let elementos = parser::parse_elementos();
-    let elementos_by_lema = parser::parse_elementos_by_lema();
+pub fn generate_words_and_pangrams() -> (Vec<(String, String)>, Vec<String>) {
+    let elements = parser::parse_elementos();
 
     println!("Filtering words and common pangrams...");
     // List of *normalized* common pangrams for puzzle generation
     let mut pangrams: HashSet<String> = HashSet::new();
     // List of all words and their lemma
     let mut words: HashSet<(String, String)> = HashSet::new();
-
-    elementos_by_lema.into_iter().for_each(|(lema, forms)| {
-        // Decide if this is a valid word
-        let Some((_, elementos_lema, categoria, frec, _, _)) = elementos.get(&lema) else {
-            return;
-        };
-        if lema != *elementos_lema {
-            // If this lema is different from the lema returned from elementos, this lema is not
-            // what we wanted. E.g. (decid, decid) picks the elemento (decid, decir), and gets
-            // included, and pollutes the puzzle data.
-            return;
-        }
-        let (valid, _) = filter(&lema, Some(&lema), Some(*categoria), *frec, true);
-        if !valid {
-            return;
-        }
-
-        // This lema is valid, so add its forms
-        forms.iter().for_each(|form| {
-            // Ensure this form is valid based on just the word, and check if its a pangram
-            let (valid, is_pangram) = filter(form, None, None, usize::MAX, true);
-            if !valid {
+    elements
+        .iter()
+        .for_each(|(_, (element, lemma, category, freq, _, _))| {
+            if lemma == &String::from("??") {
                 return;
             }
-            if is_pangram {
-                // If this form could be a pangram, grab its frequency to see if its common
-                if let Some((_, _, _, frec, _, _)) = elementos.get(form) {
-                    let (_, common_pangram) = filter(form, None, None, *frec, true);
-                    if common_pangram {
-                        pangrams.insert(unidecode::unidecode(form));
-                    }
+            // See if this is a valid word and common pangram
+            let (valid, common_pangram) =
+                filter(&element, Some(&lemma), Some(*category), *freq, true);
+            if !valid {
+                // If not valid, do a last check to see if its valid using its lemma's frequency
+                let Some((_, _, _, lemma_freq, _, _)) = elements.get(lemma) else {
+                    return;
                 };
+                let (valid_by_lemma, _) =
+                    filter(&element, Some(&lemma), Some(*category), *lemma_freq, true);
+                if !valid_by_lemma {
+                    return;
+                }
             }
-
-            words.insert((form.clone(), lema.clone()));
+            if common_pangram {
+                pangrams.insert(unidecode::unidecode(&element));
+            }
+            words.insert((element.to_string(), lemma.to_string()));
         });
-    });
 
     println!(
         "Found {} words and {} common pangrams",
         words.len(),
         pangrams.len()
     );
+
+    println!("Sorting...");
+    let mut pangrams: Vec<String> = pangrams.into_iter().collect();
+    pangrams.sort();
+    let mut words: Vec<(String, String)> = words.into_iter().collect();
+    words.sort();
+
     (words, pangrams)
 }
 
-fn write_palabras_rs(words: HashSet<(String, String)>, pangrams: HashSet<String>) {
+fn write_palabras_rs(words: Vec<(String, String)>, pangrams: Vec<String>) {
     println!("Writing palabras.rs...");
     // Words
     let mut palabras_rs = format!(
