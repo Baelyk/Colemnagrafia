b5eb0b9598bc2555f2d117189b5b970628fa01a2
Baelyk <b43lyk@gmail.com>
2024-06-02 23:26:10 +0200

    Rename to Colemnagrafia

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
-import { internalIpV4 } from "internal-ip";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
-// @ts-expect-error process is a nodejs global
-const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
-
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-	base: "spelling-bee-clone",
+	base: "Colemnagrafia",
 	plugins: [wasm(), topLevelAwait()],
 }));
